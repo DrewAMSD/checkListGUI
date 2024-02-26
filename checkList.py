ad87265c4e4862668f075db67b666bc1d5cc81b8
DrewAMSD <drewalgismiller2@gmail.com>
2024-02-26 14:16:42 -0800

    window setup

--- checkList.py
+++ checkList.py
@@ -1,7 +1,16 @@
 from tkinter import *
 
 def main():
-    print("main")
+    window = Tk() # create new tkinter window
+    window.geometry("720x620")
+    window.title("Check List")
+
+    # window icon
+    icon = PhotoImage(file="checkMark.png")
+    window.iconphoto(True, icon)
+    window.config(background="#ffffff")
+
+    window.mainloop() # display window, activate event listeners
 
 if __name__ == "__main__":
     main()
