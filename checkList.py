a2e026d00fe65084cc402127ff58c8d142f93f13
DrewAMSD <drewalgismiller2@gmail.com>
2024-02-27 13:52:35 -0800

    changed archictecture to object oriented

--- checkList.py
+++ checkList.py
@@ -1,16 +1,20 @@
-from tkinter import *
+import tkinter as tk
 
-def main():
-    window = Tk() # create new tkinter window
-    window.geometry("720x620")
-    window.title("Check List")
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("720x620")
+        self.title("Check List")
+        self.config(background="#ffffff")
+        
+        # window icon and resizing
+        icon = tk.PhotoImage(file="checkMark.png")
+        self.iconphoto(True, icon)
+        # self.resizable(False, False)
+        self.minsize(420, 260)
 
-    # window icon
-    icon = PhotoImage(file="checkMark.png")
-    window.iconphoto(True, icon)
-    window.config(background="#ffffff")
-
-    window.mainloop() # display window, activate event listeners
+        # widgets
 
 if __name__ == "__main__":
-    main()
+    root = App()
+    root.mainloop()
