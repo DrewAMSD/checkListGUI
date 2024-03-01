1ebc7de43e8c88b865ff993e518980efcd8eb21b
DrewAMSD <drewalgismiller2@gmail.com>
2024-03-01 12:46:33 -0800

    lbls and entries

--- checkList.py
+++ checkList.py
@@ -3,7 +3,7 @@ import tkinter as tk
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.geometry("720x620")
+        self.geometry("520x420")
         self.title("Check List")
         self.config(background="#ffffff")
         
@@ -11,9 +11,22 @@ class App(tk.Tk):
         icon = tk.PhotoImage(file="checkMark.png")
         self.iconphoto(True, icon)
         # self.resizable(False, False)
-        self.minsize(420, 260)
+        self.minsize(520, 420)
 
         # widgets
+        self.taskLbl = tk.Label(self,text="Task:", bg="#ffffff")
+        self.taskEntry = tk.Text(self,width=50,height=3)
+        self.dateLbl = tk.Label(self,text="Date(mm/dd/yy):", bg="#ffffff")
+        self.dateEntry = tk.Entry(self,width=20)
+        self.addTaskBtn = tk.Button(self,
+                         text="Add Task")
+
+        # place widgets
+        self.taskLbl.pack()
+        self.taskEntry.pack()
+        self.dateLbl.pack()
+        self.dateEntry.pack()
+        self.addTaskBtn.pack()
 
 if __name__ == "__main__":
     root = App()
